443d68274f48c8939ad96c75a3943ec14fb161aa
benilias <benbrahimilias@gmail.com>
2025-12-15 12:58:21 +0000

    add React routing for core pages

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,11 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Container from "./components/Container";
 import MainPage from "./pages/MainPage";
+import NavBar from "./components/NavBar";
+import AdminPage from "./pages/AdminPage";
 
 function App() {
   return (
+    <BrowserRouter>
     <Container>
-     <MainPage />
+      <NavBar />
+     <Routes>
+      <Route path="/" element={<MainPage />}></Route>
+      <Route path="/admin" element={<AdminPage />}></Route>
+     </Routes>
     </Container>
+    </BrowserRouter>
     
   );
 }
